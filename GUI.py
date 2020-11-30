0bb75a1a324a55fff0b24fd990391df884765c76
Alvin saini <codewithalvin@gmail.com>
2020-11-30 12:13:00 +0530

    some bugs fixed introduce oops

--- GUI.py
+++ GUI.py
@@ -5,57 +5,51 @@ from PIL import ImageTk,Image
 
 
 
-class App:
+class GUI(Tk):
 	def __init__(self):
-		self.root=Tk()
-				
-		#Setting Variables
-		
-		#Display Screen
-		self.Settings()
-		self.Menu()
-		self.CanvasWindow()
-		
-		
-		self.root.mainloop()
-	
-	def Settings(self):
-		self.root.config(bg="black")
-		self.root.geometry("1040x1900")
-		self.root.title("Alvin's PhotoShop")
+		super().__init__()
+		self.config(bg="black")
+		self.geometry("1040x1900")
+		self.title("Alvin's PhotoShop")
 	 
 	def Menu(self):
 		#=========creating main menu=========#
-		mainMenu=Menu(self.root)
+		self.mainMenu=Menu(self)
 		
 		#========creating sub menus==========#
-		m1=Menu(mainMenu)
+		self.m1=Menu(self.mainMenu)
 
-		mainMenu.add_cascade(label="Files",menu=m1)
-		mainMenu.add_command(label="Export")
+		self.mainMenu.add_cascade(label="Files",menu=self.m1)
+		self.mainMenu.add_command(label="Export")
 
 		#====adding command in menu m1=====#
-		m1.add_command(label="Open",command=self.HackImage)
-		m1.add_command(label="Settings")
-		m1.add_command(label="Clear",command=lambda: canvas.delete("all"))
+		self.m1.add_command(label="Open",command=self.HackImage)
+		self.m1.add_command(label="Settings")
+		self.m1.add_command(label="Clear",command=lambda: canvas.delete("all"))
 		
 		#=========configing Menu===========#
-		self.root.config(menu=mainMenu)
+		self.config(menu=self.mainMenu)
 	
 	def CanvasWindow(self):
-		self.canvas=Canvas(width=1040,height=1700,bg="white")
+		self.canvas=Canvas(self,width=1040,height=1700,bg="white")	
 		self.canvas.pack()
-		pic=Image.open("/storage/emulated/0/DCIM/Screenshots/IMG_20201126_084733.jpg")
-		img = ImageTk.PhotoImage(pic)
-		self.canvas.create_image(500,500,image=img)
-		  	
+		
 
 	#Functions 
 	def HackImage(self):	
-		image=askopenfile(mode='r', filetypes=[('Select an Image', '.jpg  .png .jpeg')])
-		pic=Image.open("/storage/emulated/0/DCIM/Screenshots/IMG_20201126_084733.jpg")
-		img = ImageTk.PhotoImage(pic)
-		self.canvas.create_image(500,500,anchor="nw",image=img)
+		self.image=askopenfile(mode='r', filetypes=[('Select an Image', '.jpg  .png .jpeg')])
+		self.pic=Image.open(self.image.name)
+		self.img = ImageTk.PhotoImage(self.pic)
+		self.canvas.create_image(0,0,anchor="nw",image=self.img)
 		
 
-App()
+if __name__=="__main__":
+	app=GUI()
+	
+	#runnings methods
+	app.Menu()
+	app.CanvasWindow()
+	
+	#running mainloop
+	app.mainloop()
+	
